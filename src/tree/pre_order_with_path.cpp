de9062f5321ef4ee52cb4f5b6fbfbdf51019c13f
hqjiang <jhq13@foxmail.com>
2020-11-29 11:48:30 +0800

    alg# add path handlers

--- src/tree/pre_order_with_path.cpp
+++ src/tree/pre_order_with_path.cpp
@@ -1,57 +1,73 @@
 #include "util.cpp"
-
-int preorderTraversal(TreeNode *root)
-{
-    stack<TreeNode *> path;
-    TreeNode *curr = root;
-    while (curr)
-    {
-        //visit current node;
-        cout << curr->val << ", ";
-
-        //visit left subtree
-        if (curr->left)
+class Solution {
+public:
+    int tsum;
+    vector<TreeNode *> path;
+    vector<int> vals;
+    void add_path(TreeNode* node){
+        //actions when new path is found
+        tsum+=node->val;
+        path.push_back(node);
+        vals.push_back(node->val);
+    }
+    void rm_path(){
+        //actions when retreive from path
+        tsum-= *vals.rbegin();
+        path.pop_back();
+        vals.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        TreeNode *curr = root;
+        vector<vector<int>> ret;
+        while (curr)
         {
-            path.push(curr);
-            curr = curr->left;
-        }
-        //visit right subtree
-        else if (curr->right)
-        {
-            path.push(curr);
-            curr = curr->right;
-        }
-        else
-        {
-            //curr is leaf node
-            TreeNode *last = nullptr;
-            while (1)
+            //visit current node;
+            cout << curr->val << ", ";
+            
+            //visit left subtree
+            if (curr->left)
             {
-                if (last == curr->right)
+                add_path(curr);
+                curr = curr->left;
+            }
+            //visit right subtree
+            else if (curr->right)
+            {
+                add_path(curr);
+                curr = curr->right;
+            }
+            else
+            {
+                //curr is leaf node
+                TreeNode *last = nullptr;
+                while (1)
                 {
-                }
-                else if (last == curr->left)
-                {
-                    if (curr->right)
-                    {
-                        path.push(curr);
-                        curr = curr->right;
-                        break;
-                    }
-                    else
+                    if (last == curr->right)
                     {
                     }
+                    else if (last == curr->left)
+                    {
+                        if (curr->right)
+                        {
+                            add_path(curr);
+                            curr = curr->right;
+                            break;
+                        }
+                        else
+                        {
+                        }
+                    }
+                    if (curr == root)
+                        return ret;
+                    last = curr;
+                    curr = *path.rbegin();
+                    rm_path();
                 }
-                if (curr == root)
-                    return ret;
-                last = curr;
-                curr = path.top();
-                path.pop();
             }
         }
-    }
     return ret;
-}
+    }
+};
 int main(){
     TreeNode *root = new TreeNode(4, new TreeNode(9, new TreeNode(5, nullptr, nullptr), new TreeNode(1, nullptr, nullptr)), new TreeNode(0, nullptr, nullptr));
     auto res=postorderTraversal(root);
